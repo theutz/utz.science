918aa39ed376a6cdebd6ac3285591238dd202f61
Michael Utz <michael@theutz.com>
2018-11-28 23:24:18 +0200

    chore: setup now

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development' ? {} : require('next-server/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    return {
+      /* production only config */
+    }
+  }
+
+  const withTypescript = require('@zeit/next-typescript')
+
+  return withTypescript()
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Index = () => <div>Hello</div>
 
 export default Index
