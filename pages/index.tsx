c34eaf016f70785059c975fc84203b748917145f
Michael Utz <michael@theutz.com>
2018-11-28 23:30:35 +0200

    style: basic colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
-const Index = () => <div>Hello</div>
+const Index = () => (
+  <div
+    style={{
+      backgroundColor: '#44344F',
+      color: '#EAE0D5',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <h1>Dr. Begüm Utz, Ph.D.</h1>
+  </div>
+)
 
 export default Index
